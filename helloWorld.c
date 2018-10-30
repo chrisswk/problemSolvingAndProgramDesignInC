208f333f91330fd33572cc4bad9ae1c849cd3742
Christopher Kramlich <chris@kramlich.com>
2018-10-30 16:28:06 -0700

    Making directories for projects

--- helloWorld.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int
-main(void)
-{
-	printf("Hello, world and happy 34th birthday, Christopher!\n");
-	return (0);
-}
---
+++ helloWorld/helloWorld.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int
+main(void)
+{
+	printf("Hello, world and happy 35th birthday, Christopher!\n");
+	return (0);
+}
+
+
+$$tarw@rscantb3Sto0pp3d
+
+Wew@ntthefun$11
+
+He7ecom3syourman@@99
+
+H0pskip@ndajUm&22
+
