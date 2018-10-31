34e13d85e7a689218f4a5391cb2a341dc752a58e
Christopher Kramlich <chris@kramlich.com>
2018-10-30 17:10:59 -0700

    Relearn branching

---
+++ milesToKMConversion/milesToKMConversion.c
@@ -0,0 +1,25 @@
+/*
+*  Converts distances from miles to kilometers.
+*/
+
+#include <stdio.h>  /* printf, scanf definitions */
+#define KMS_PER_MILE 1.609 /* conversion constant */
+
+int
+main(void)
+{
+  double miles, /* distance in miles */
+	 kms; /* equivalent distance in kilometers */
+  
+  /* Get the distance in miles. */
+  printf("Enter the distance in miles> ");
+  scanf("%lf", &miles);
+
+  /* Convert the distance to kilometers.  */
+  kms = KMS_PER_MILE * miles;
+
+  /* Display the distance in kilometers. */
+  printf("That equals %f kilometers. \n", kms);
+
+  return 0;
+}
